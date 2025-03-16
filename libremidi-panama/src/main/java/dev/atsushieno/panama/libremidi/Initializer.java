bee6a239114f9277daebf0e8208eb3721d021a5c
Atsushi Eno <atsushieno@gmail.com>
2025-03-16 12:18:20 +0800

    forgot that Java methods are private by default.

--- libremidi-panama/src/main/java/dev/atsushieno/panama/libremidi/Initializer.java
+++ libremidi-panama/src/main/java/dev/atsushieno/panama/libremidi/Initializer.java
@@ -3,7 +3,7 @@ package dev.atsushieno.panama.libremidi;
 import com.fizzed.jne.JNE;
 
 public class Initializer {
-    static void initialize() {
+    public static void initialize() {
         JNE.loadLibrary("libremidi");
     }
 }

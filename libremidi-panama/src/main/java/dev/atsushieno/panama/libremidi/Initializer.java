4a500ba481cf74f81889e5bc207d6a21deacd807
atsushieno <atsushieno@gmail.com>
2025-03-29 11:50:25 +0000

    Deploying to gh-pages from @ atsushieno/libremidi-panama@b9373db6fbab9f5f9f23a276398d82774d156228 🚀

--- libremidi-panama/src/main/java/dev/atsushieno/panama/libremidi/Initializer.java
+++
@@ -1,9 +0,0 @@
-package dev.atsushieno.panama.libremidi;
-
-import com.fizzed.jne.JNE;
-
-public class Initializer {
-    public static void initialize() {
-        JNE.loadLibrary("libremidi");
-    }
-}
